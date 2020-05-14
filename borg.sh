837eed50bdbe8424f8b6a146df976319872ecdb8
Houjun Liu <hliu@shabang.cf>
2020-05-13 17:10:28 -0700

    Tmux themeing

--- borg.sh
+++ borg.sh
@@ -160,7 +160,7 @@ echo
 echo "Chapter 4: copying config files"
 echo "================================"
 echo -n "Copying the config profiles... "
-(cp Borg/src/.tmux.conf ~/; cp Borg/src/.zprofile ~/; cp Borg/src/.zshrc ~/; cp Borg/src/.tmux.conf ~/; mkdir .tmux; mkdir ~/.config; mkdir ~/.config/nvim; cp Borg/src/nvim/init.vim ~/.config/nvim/; cp -r Borg/src/nvim/UltiSnips ~/.config/nvim/; mkdir ~/.tmux/; cp Borg/src/airline.conf ~/.tmux/; cp Borg/src/.p10k.zsh ~/) &> /dev/null &
+(cp Borg/src/.tmux.conf ~/; cp Borg/src/.zprofile ~/; cp Borg/src/.zshrc ~/; cp Borg/src/.tmux.conf ~/; mkdir .tmux; mkdir ~/.config; mkdir ~/.config/nvim; cp Borg/src/nvim/init.vim ~/.config/nvim/; cp -r Borg/src/nvim/UltiSnips ~/.config/nvim/; mkdir ~/.tmux/; cp Borg/src/.p10k.zsh ~/) &> /dev/null &
 spinner $!
 wait $!
 echo -e "\rCopying the config profiles... Done."
@@ -221,6 +221,11 @@ echo -n "Sourcing tpm plugins... "
 spinner $!
 wait $!
 echo -e "\rSourcing tpm plugins... Done."
+echo -n "Generating tmux theme..."
+(nvim -c "Tmuxline airline" &> /dev/null; nvim -c "TmuxlineSnapshot ~/.tmux/airline.conf" &> /dev/null) &
+spinner $!
+wait $!
+
 clear
 echo
 echo "Borg v0.0.1. We assimilated (your shell)."
